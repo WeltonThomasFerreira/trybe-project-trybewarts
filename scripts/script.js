646061700a53dc94ab9558b673c7457d44cff995
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-21 18:32:47 -0300

    Requisitos 18 e 19

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,17 @@
 const loginButton = document.querySelector('#login-button');
+const checkBox = document.querySelector('#agreement');
+const submitButton = document.querySelector('#submit-btn');
+
+checkBox.addEventListener('click', () => {
+  if (checkBox.checked === true) {
+    submitButton.removeAttribute('disabled');
+    console.log(checkBox.value);
+  } else {
+    submitButton.setAttribute('disabled', 'disabled');
+    console.log(checkBox.value);
+  }
+});
+
 loginButton.addEventListener('click', (event) => {
   event.preventDefault();
   const login = document.querySelector('#input-login');
