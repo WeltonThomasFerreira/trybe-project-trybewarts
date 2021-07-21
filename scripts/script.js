7497e59c9d60e8dd9262dafadb4b5b4b038cec30
Welton Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-07-21 15:56:09 -0300

    Implementa parcialmente o requisito 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-console.log('Você é um bruxo, Harry');
+const loginButton = document.querySelector('#login-button');
+loginButton.addEventListener('click', () => {
+  const login = document.querySelector('#input-login');
+  const password = document.querySelector('#input-password');
+  console.log(login.value);
+  console.log(password.value);
+  if (login.value === 'tryber@teste.com' && password.value !== '123456') {
+    window.alert('Olá, Tryber!');
+  }
+});
